66f9b0408cc6eb413100e6d9bed9741484a884ac
Grecia Fragoso <naomistereo96@gmail.com>
2023-09-25 10:50:19 -0500

    New promise, can read path, verify if is absolute (transform into absolute if isnt), read file and print in console

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-module.exports = () => {
-  // ...
-};
+// module.exports = () => {
+//   // aquí va mi función mdLinks
+// }
+
+// const path = require('node:path');
+const { mdLinks } = require('./lib/mdLinks')
+// const fs = require('fs');
+ 
+console.log('Index executing');
+
+mdLinks('./prueba.txt')
+  .then(links => {
+    console.log('Promise ok')
+    // => [{ href, text, file }, ...]
+  })
+  .catch(console.error);
+
+ module.exports = () => mdLinks;
