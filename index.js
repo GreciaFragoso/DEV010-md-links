cb4db437fa5297ba068030483cee3919dafe4c3f
Grecia Fragoso <naomistereo96@gmail.com>
2023-09-26 13:48:23 -0500

    can read an md file, transform into a HTML file and get links

--- index.js
+++ index.js
@@ -4,13 +4,17 @@
 
 // const path = require('node:path');
 const { mdLinks } = require('./lib/mdLinks')
+// const { transformToHTML } = require('./lib/transformToHTML');
 // const fs = require('fs');
+const { findLinks } = require('./lib/findLinks');
  
-console.log('Index executing');
+// console.log('Index executing');
 
 mdLinks('./prueba.md')
   .then(() => {
     console.log('Promise ok')
+    findLinks('./prueba.md')
+    // transformToHTML();
     // => [{ href, text, file }, ...]
   })
   .catch(console.error);
--- prueba.md
+++ prueba.md
@@ -1,2 +1,7 @@
 holaaaaa
 este archivo es de prueba md
+Click [aquí](https://www.linkedin.com/in/grecia-naomi-fragoso-mart%C3%ADnez-513655138/) para ir a mi perfil de LinkedIn.
+
+Click [aquí](https://concepto.de/wp-content/uploads/2019/05/aurora-boreal-polar-e1559012098292.jpg) para ver la foto de una aurora boreal.
+
+O, si tienes ganas de aprender la diferencia entre promesas, callbacks y async-await, mira [este tutorial](https://www.youtube.com/watch?v=HgqNstf4xg0).
