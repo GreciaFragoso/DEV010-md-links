c4424c71350c56d1fffb15368d2e3b71d4876461
Grecia Fragoso <naomistereo96@gmail.com>
2023-09-27 14:22:14 -0500

    tests for find links function

---
+++ test/findLinks.spec.js
@@ -0,0 +1,13 @@
+const { findLinks } = require('../lib/findLinks');
+
+describe('Find links', () => {
+    it('Should return prueba.md stats', () => {
+        const usersPath = 'C:\\Users\\Grecia\\Desktop\\Curso Python\\Bootcamp Laboratoria\\MD - Links\\DEV010-md-links\\prueba.md';
+        const consoleSpy = jest.spyOn(console, 'log');
+
+        return findLinks(usersPath).then(links => {
+            expect(consoleSpy).toHaveBeenCalled();
+            expect(links.length).toBe(3);
+        })
+    })
+})
