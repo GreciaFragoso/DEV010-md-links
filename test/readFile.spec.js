8e71cf09c265d2234a23217eb5f31fc15e938a16
Grecia Fragoso <naomistereo96@gmail.com>
2023-09-27 20:16:57 -0500

    test for read a file function

--- test/readFile.spec.js
+++ test/readFile.spec.js
@@ -3,22 +3,36 @@ const fs = require('fs');
 // const { transformToHTML } = require('../lib/transformToHTML');
 // const { transformToHTML } = require('../lib/transformToHTML');
 
-global.console = { log: jest.fn() };
+// global.console = { log: jest.fn() };
 
 describe('readFile', () => {
     it('Should read a file', async () => {
-        const usersPath = './pruebatest.md'
+        const usersPath = 'C:\\Users\\Grecia\\Desktop\\Curso Python\\Bootcamp Laboratoria\\MD - Links\\DEV010-md-links\\pruebatest.md';
         const mockFileContent = 'This is a test';
         jest.spyOn(fs, 'readFile').mockImplementation((path, options, callback) => {
-            callback(null, mockFileContent)
+             callback(null, mockFileContent)
         })
         const transformToHTML = jest.fn().mockResolvedValue('<p>This is a test</p>');
-        // const dataHTML = transformToHTML(data);
+        //const dataHTML = transformToHTML(data);
         const myPromise = readAFile(usersPath, transformToHTML);
         return myPromise.then((result) => {
-            expect(result).toBe('<p>This is a test</p>');
+            expect(result).toMatch('<p>This is a test</p>');
         })
-  })
+    })
+    it('Should reject the promise', () => {
+        const usersPath = 'C:\\Users\\Grecia\\Desktop\\Curso Python\\Bootcamp Laboratoria\\MD - Links\\DEV010-md-links\\pruebatest.md';
+        // const mockFileContent = 'This is a test';
+        const error = new Error();
+        // const consoleSpy = jest.spyOn(console, 'log');
+        jest.spyOn(fs, 'readFile').mockImplementation((path, options, callback) => {
+             callback(error)
+        })
+        const myPromise = readAFile(usersPath);
+        return myPromise.catch((result) => {
+            // expect(consoleSpy).toHaveBeenCalledWith('Something went wrong', error);
+            expect(result).toBe(error);
+        })
+    })
 })
 // it('Should read a file', async () => {
 //     const usersPath = './prueba.md'
