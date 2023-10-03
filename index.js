697d2ce499a604f6fe5e61d0eecb92d500806dce
Grecia Fragoso <naomistereo96@gmail.com>
2023-10-03 12:23:05 -0500

    readAdirectory function

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-const { mdLinks } = require('./lib/mdLinks')
+const { mdLinks } = require('./lib/mdLinks');
+const { readADirectory } = require('./lib/readADirectory');
+// const path = require('path');
 // const { findLinks } = require('./lib/findLinks');
  
-// console.log('Index executing');
+// console.log(path.);
 const userPath = './prueba.md';
 const validate = true;
+// const userDirectory = 'C:\\Users\\Grecia\\Desktop\\Curso\ Python\\Bootcamp\ Laboratoria\\MD\ -\ Links\\DEV010-md-links\\pruebas'
+// const userDirectory = 'C:\Users\Grecia\Desktop\Curso Python\Bootcamp Laboratoria\MD - Links\DEV010-md-links\pruebas'
+const userDirectory = './pruebas'
 
-
+readADirectory(userDirectory);
 mdLinks(userPath, validate)
   .then((result) => {
     // console.log(result)
     // const userAbsolutePath = mdLinks(userPath)
     console.log(result);
+    // readADirectory(userDirectory)
     // transformToHTML();
     // => [{ href, text, file }, ...]
   })
