5ffe945b082d50bbea0ca93ddc3adf20683554ec
Grecia Fragoso <naomistereo96@gmail.com>
2023-10-03 19:57:13 -0500

    read a directory function, find links for every md file

--- index.js
+++ index.js
@@ -1,24 +1,15 @@
 const { mdLinks } = require('./lib/mdLinks');
-const { readADirectory } = require('./lib/readADirectory');
-// const path = require('path');
-// const { findLinks } = require('./lib/findLinks');
- 
-// console.log(path.);
-const userPath = './prueba.md';
-const validate = true;
-// const userDirectory = 'C:\\Users\\Grecia\\Desktop\\Curso\ Python\\Bootcamp\ Laboratoria\\MD\ -\ Links\\DEV010-md-links\\pruebas'
-// const userDirectory = 'C:\Users\Grecia\Desktop\Curso Python\Bootcamp Laboratoria\MD - Links\DEV010-md-links\pruebas'
-const userDirectory = './pruebas'
+// const { readAllFiles } = require('./lib/readADirectory');
 
-readADirectory(userDirectory);
+// const userPath = './prueba.md';
+const userPath = './pruebas';
+const validate = true;
+// const userDirectory = './pruebas'
+ 
+// readAllFiles(userDirectory);
 mdLinks(userPath, validate)
   .then((result) => {
-    // console.log(result)
-    // const userAbsolutePath = mdLinks(userPath)
-    console.log(result);
-    // readADirectory(userDirectory)
-    // transformToHTML();
-    // => [{ href, text, file }, ...]
+    // console.log(result); este console.log daba undefined
   })
   .catch(console.error);
 
--- pruebas/prueba1.md
+++ pruebas/prueba1.md
@@ -1,1 +1,4 @@
-hola
+hola, en este [link](https://lucid.app/lucidchart/d161b083-b971-413b-8904-d1a7799c880e/edit?invitationId=inv_897e60fe-7b01-41f2-9635-150738672a2a&page=0_0#) se encuentra el diagrama de flujo del proyecto
+
+En este otro [lugar](https://github.com/Laboratoria/DEV010-md-links/blob/main/docs/03-milestone.md) está el hito-3 
+del proyecto, el cual estoy solucionando en este momento.
